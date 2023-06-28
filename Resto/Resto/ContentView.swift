cecfaec4c4f66817527867387d6e6ad2c57ba4e4
admin <admin@Mac-mini-Vladimir-2.local>
2023-06-28 11:50:48 +0400

    first commit

---
+++ Resto/Resto/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Resto
+//
+//  Created by admin on 28.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Resto/Resto/RestoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestoApp.swift
+//  Resto
+//
+//  Created by admin on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
